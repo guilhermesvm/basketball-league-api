07476d060baef3d823911f78a4f4b4521be95169
Guilherme Machado <guilhermesvmachado@gmail.com>
2024-09-20 20:37:37 -0300

    feat: add Team Routes

---
+++ src/routes/teamRoute.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { TeamController } from "../controllers/teamController";
+import { validationMiddleware } from "../middleware/fieldValidation";
+import TeamEntity from "../entities/Team";
+
+const router = express.Router();
+const teamController = new TeamController();
+
+router.get("/teams", teamController.getAll);
+router.get("/teams/:id", teamController.getById);
+router.post("/teams", validationMiddleware(TeamEntity), teamController.create);
+router.put("/teams/:id", validationMiddleware(TeamEntity), teamController.update);
+router.delete("/teams/:id", teamController.delete);
+
+export default router;
