c143730c6f5cdd41d2b3595d8114cf202400e0ad
Guilherme Machado <guilhermesvmachado@gmail.com>
2024-09-20 20:38:21 -0300

    fix: include team router on app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import errorHandler from "./middleware/errorHandler";
 import healthRoutes from "./routes/healthRoute";
 import playerRoutes from "./routes/playerRoute";
+import teamRoutes from "./routes/teamRoute";
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors());    
+app.use(cors());
 
 app.use("/webmob", healthRoutes);
 app.use("/webmob", playerRoutes);
+app.use("/webmob", teamRoutes);
 
 app.use(errorHandler);
 
