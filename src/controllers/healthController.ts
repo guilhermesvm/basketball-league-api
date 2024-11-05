175cf564d5fedb021a1906200288d8d5952da76b
Guilherme Machado <guilhermesvmachado@gmail.com>
2024-11-05 14:41:36 -0300

    fix: healthController

--- src/controllers/healthController.ts
+++ src/controllers/healthController.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
 
 export class HealthController {
-    get = async(req: Request, res: Response): Promise<void> => {
-        res.status(200).json({message: "Success."});
+    health = async(req: Request, res: Response): Promise<void> => {
+        const healthCheck = {
+            message: "Success.",
+            timestamp: Date.now()
+        };
+        
+        try {
+            res.status(200).json(healthCheck)
+        } catch (error: any) {
+            healthCheck.message = error;
+            res.status(503).json();
+        }
     }
 }
